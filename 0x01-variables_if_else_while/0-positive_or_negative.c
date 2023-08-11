1063a78da95ec2a6eabe20641bd539c39a068878
Abdullahi Adeniran <princejolade@gmail.com>
2023-08-11 13:44:56 -0700

    Fixed extra whitespace for 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
 /**
  * main - Entry point
  *
- * Description: "The program's description" 
+ * Description: "The program's description"
  *
  * Return: Always 0 (Success)
  */
